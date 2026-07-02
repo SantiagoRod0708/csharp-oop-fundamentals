6749f1c51b484b5acd808ac0b9ace592823e2359
SantiagoRod0708 <tiagomaster2001@gmail.com>
2026-07-02 12:58:29 -0600

    Update Vehiculo.cs

--- Herencia/Vehiculo.cs
+++ Herencia/Vehiculo.cs
@@ -1,1 +1,19 @@
+namespace Herencia
+{
+    internal class Vehiculo
+    {
+        private string color;
+        private int año, numPuerta;
 
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Color { get => color; set => color = value; }
+        public int Año { get => año; set => año = value; }
+        public int NumPuerta { get => numPuerta; set => numPuerta = value; }
+
+        public virtual string MostrarInformacion() 
+        {
+            return $"\r\n Marca: {Marca} \r\n Modelo: {Modelo} \r\n Año: {Año} \r\n Color: {Color}";
+        }
+    }
+}
